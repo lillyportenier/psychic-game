7e334059790bccfd90fdc99556810d72027b396f
laportenier <lilly.portenier@gmail.com>
2019-09-26 16:43:15 -0400

    adding multiople keystrokes to the string of letters guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
 var guessesLeft = 10;
 var wins = 0;
 var addLetter;
+var userInput;
+var lettersGuessed = [];
+
 
 
 // computer generates random lettter
@@ -12,12 +15,27 @@ var computerGuess =
 console.log(computerGuess);
 
 // cath user key stroke
-var userGuess = document.getElementById("userGuess");
-
 document.onkeyup = function(event){
-    userGuess.textContent = event.key;
-};
-// put user input on screen 
+    var keyPress = (String.fromCharCode(event.keyCode)).toLowerCase();
+    addLetter(keyPress);
+}
+
+// add user guess to array 
+function addLetter(userKeypress) {
+    var repeatGuess = lettersGuessed.some(function(item){
+        return item === userKeypress;
+    })
+   lettersGuessed.push(userKeypress);   
+}
+// show letters guessed in browser
+function showLetters() {
+    var guessesString = 
+}
+    
+
+
+
+
 
 // check if user input is correct
 // if correct push win ++ to desktop
